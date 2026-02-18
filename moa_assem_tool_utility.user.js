9371457527f29706f9ba555a62b053feace2973d
armored-core <>
2026-02-18 14:02:56 +0900

    style: インデント調整

--- moa_assem_tool_utility.user.js
+++ moa_assem_tool_utility.user.js
@@ -10,30 +10,30 @@
 // ==/UserScript==
 
 (function () {
-    'use strict';
+  "use strict";
 
-    const formControl = document.forms['Control'];
-    const assembleTemplate = document.createElement('select');
-    const robots = [
-        { name: 'ドラム腕デビガン', code: '[5rbokQeR,oA0h]' },
-        { name: 'Eサブデビガン', code: '[VjaPQKac,oA1p]' },
-        { name: '陸リヴァイアサン', code: '[16AoCYnv2,nJtb]' },
-        { name: '見た目はお嬢様', code: '[5qFQn4X7,oA0h]' },
-    ];
-    assembleTemplate.setAttribute('id', 'assemble-template');
-    assembleTemplate.innerHTML = '<option data-assem-code="">-</option>';
-    robots.forEach((val) => {
-        assembleTemplate.innerHTML += `<option value="${val.code}">${val.name}</option>"`;
+  const formControl = document.forms["Control"];
+  const assembleTemplate = document.createElement("select");
+  const robots = [
+    { name: "ドラム腕デビガン", code: "[5rbokQeR,oA0h]" },
+    { name: "Eサブデビガン", code: "[VjaPQKac,oA1p]" },
+    { name: "陸リヴァイアサン", code: "[16AoCYnv2,nJtb]" },
+    { name: "見た目はお嬢様", code: "[5qFQn4X7,oA0h]" },
+  ];
+  assembleTemplate.setAttribute("id", "assemble-template");
+  assembleTemplate.innerHTML = '<option data-assem-code="">-</option>';
+  robots.forEach((val) => {
+    assembleTemplate.innerHTML += `<option value="${val.code}">${val.name}</option>"`;
+  });
+  formControl.insertBefore(
+    assembleTemplate,
+    document.getElementById("AssyCode"),
+  );
+
+  document
+    .getElementById("assemble-template")
+    .addEventListener("change", function () {
+      formControl.AssyCode.value = this.value;
+      formControl.AssyCodeB.click();
     });
-    formControl.insertBefore(
-        assembleTemplate,
-        document.getElementById('AssyCode')
-    );
-
-    document
-        .getElementById('assemble-template')
-        .addEventListener('change', function () {
-            formControl.AssyCode.value = this.value;
-            formControl.AssyCodeB.click();
-        });
 })();
